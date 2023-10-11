9b3f0b93970c0f25c4ee7dc77a53b38ffb409908
Weffy61 <weffy@mail.ru>
2023-10-12 02:06:08 +0300

    Raise exception for redirection

--- main.py
+++ main.py
@@ -2,24 +2,41 @@ import requests
 import os
 
 
-def download_books():
-    for book_num in range(10):
-        payload = {
-            'id': book_num + 1
-        }
+def get_book(book_id):
+    payload = {
+        'id': book_id + 1
+    }
 
-        file_name = f'{book_num + 1}.txt'
-        url = "https://tululu.org/txt.php"
 
-        response = requests.get(url, params=payload)
-        response.raise_for_status()
-        with open(f'books/{file_name}', 'wb') as file:
-            file.write(response.content)
+    url = "https://tululu.org/txt.php"
+
+    response = requests.get(url, params=payload, allow_redirects=True)
+    response.raise_for_status()
+    return response
+
+
+def download_book(response, book_id):
+    file_name = f'id_{book_id + 1}.txt'
+    with open(f'books/{file_name}', 'wb') as file:
+        file.write(response.content)
+
+
+def check_for_redirect(response):
+    if response.history:
+        raise requests.HTTPError('выполнена переадресация со страницы книги')
 
 
 def main():
-    os.makedirs("books", exist_ok=True)
-    download_books()
+    DIR_NAME = 'books'
+    os.makedirs(DIR_NAME, exist_ok=True)
+    for book_num in range(10):
+        try:
+            response = get_book(book_num)
+            check_for_redirect(response)
+            download_book(response, book_num)
+        except requests.exceptions.HTTPError as ex:
+            print(f'Книга с id {book_num + 1} недоступна, так как {ex}')
+            continue
 
 
 if __name__ == '__main__':
