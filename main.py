8be01fb94a94a60a833aae84b10a3c91dc8709f9
Weffy61 <weffy@mail.ru>
2024-02-19 20:28:24 +0300

    Integrate livereload

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import json
-from http.server import HTTPServer, SimpleHTTPRequestHandler
 
+from livereload import Server
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 
-def main():
+def on_reload():
     env = Environment(
         loader=FileSystemLoader('.'),
         autoescape=select_autoescape(['html', 'xml'])
@@ -28,8 +28,11 @@ def main():
     with open('index.html', 'w', encoding="utf8") as file:
         file.write(rendered_page)
 
-    server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-    server.serve_forever()
+
+def main():
+    server = Server()
+    server.watch('template.html', on_reload)
+    server.serve(root='.')
 
 
 if __name__ == '__main__':
